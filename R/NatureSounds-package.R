d3382d95ab8301a800b3a233d07fbf2c0a8f460c
Marcelo Araya-Salas <araya-salas@cornell.edu>
2018-02-09 12:31:48 +0000

    version 1.0.0

---
+++ R/NatureSounds-package.R
@@ -0,0 +1,19 @@
+#' AnimalSounds: Animal Sounds for Bioacustic Analysis
+#' 
+#' NatureSounds is a package containing a collection of example animal sounds for bioacoustic analysis.
+#' The main goal is to provide example recordings to faciliate the development of bioacoustic 
+#' tools in the R environment. When possible, selection tables with the time/frequency coordinates of the sounds 
+#' in the example recordings will also be included. Note that this package does 
+#' not contain any function. Check out the packages 'warbleR', 'tuneR' and 'seewave' 
+#' for sound file manipulation and analysis in R.  
+#' 
+#' @author Marcelo Araya-Salas
+#'   
+#'   Maintainer: Marcelo Araya-Salas (\email{araya-salas@@cornell.edu})
+#'   
+#' @docType package
+#' @name NatureSounds
+#' @details License: GPL (>= 2)  
+NULL
+#> NULL 
+#'
